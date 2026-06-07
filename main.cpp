d717720f7366499758dded6208ade8dfdf84c66b
finalSay9 <chimwazaevan1@gmail.com>
2026-06-07 14:45:24 +0200

    setting up the environment to learn c++

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "enter your name (followed by enter)";
+    std::string name;
+    std::cin >> name;
+    std::cout<<"hello"<<name<<"";
+    return 0;
+}
