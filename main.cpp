c27bd6a3650b88173eec98d6ab2df6bf1095854e
finalSay9 <chimwazaevan1@gmail.com>
2026-06-07 15:05:38 +0200

    setting up the environment to learn c++

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <string>
+#include <cstdint>
 
 int main() {
-    std::cout << "enter your name (followed by enter)";
+    std::cout << "TechVac Bank\n";
+
     std::string name;
+    int16_t age;
+    std::string address;
+
+    std::cout << "How can we help you?\n";
+
+    std::cout << "Enter your name: ";
     std::cin >> name;
-    std::cout<<"hello"<<name<<"";
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    std::cin.ignore(); // important before getline
+
+    std::cout << "Enter your address: ";
+    std::getline(std::cin, address);
+
+    std::cout << "\n--- Summary ---\n";
+    std::cout << "Name: " << name << "\n";
+    std::cout << "Age: " << age << "\n";
+    std::cout << "Address: " << address << "\n";
+
     return 0;
 }
